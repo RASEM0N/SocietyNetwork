e3e1461e8589923f86f953f47213644150e6236c
RASEM0N <70789810+RASEM0N@users.noreply.github.com>
2021-02-25 01:45:20 +0500

    Connect to Database

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,38 @@
-/* Import */
+//#region Imports
 const express = require('express')
 const colors = require('colors')
+const connectDB = require('./config/db')
+const dotenv = require('dotenv')
+
+// Middleware
+const morgan = require('morgan')
+
+//#endregion
+
+// Load env vars
+dotenv.config({ path: './config/config.env' })
+
+// Connect Database
+connectDB()
+
 const app = express()
 
-const PORT = process.env.PORT || 500
-
+//#region Routes
 app.get('/', (req, res) => {
     res.send('API Running')
 })
 
+//#endregion
+
+//#region Middleware
+// Dev logging middleware
+if (process.env.NODE_ENV === 'development') {
+    app.use(morgan('dev'))
+}
+
+//#endregion
+
+const PORT = process.env.PORT || 5000
 app.listen(PORT, () => {
-    console.log(`------------------------------------------`)
     console.log(`Server started on port ${PORT}`.yellow.bold)
 })
