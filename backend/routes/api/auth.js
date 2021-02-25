153fcf9e8887342cc1931c0c3ee7a6a6ff6c89cd
RASEM0N <70789810+RASEM0N@users.noreply.github.com>
2021-02-25 20:29:41 +0500

    Custom Middlware. Auth JWT

--- backend/routes/api/auth.js
+++ backend/routes/api/auth.js
@@ -1,12 +1,25 @@
-const express = require('express')
-const router = express.Router()
-
+const express = require('express');
+const router = express.Router();
+const auth = require('../../middleware/auth');
+const User = require('../../models/User');
 
 // @route           GET api/auth
 // @description     Test Route
 // @access          Public
-router.route('/').get((req,res) => {
-    res.send(`Auth rout`)
-})
+router.route('/').get(auth, async (req, res, next) => {
+    try {
+        const user = await User.findById(req.user.id);
+        await res.status(200).json({
+            success: true,
+            data: user,
+        });
+    } catch (e) {
+        console.error(e);
+        await res.status(400).json({
+            success: false,
+            msg: `${e.message}`,
+        });
+    }
+});
 
-module.exports = router
+module.exports = router;
