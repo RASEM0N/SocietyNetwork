ab6d096680c1e617af81637f5656b174e01cc740
RASEM0N <70789810+RASEM0N@users.noreply.github.com>
2021-02-26 00:44:31 +0500

    Get current User Profile

--- backend/routes/api/profile.js
+++ backend/routes/api/profile.js
@@ -1,12 +1,40 @@
-const express = require('express')
-const router = express.Router()
+const express = require('express');
+const router = express.Router();
+const auth = require('../../middleware/auth');
 
+const Profile = require('../../models/Profile');
+const User = require('../../models/User');
 
-// @route           GET api/profile
-// @description     Test Route
-// @access          Public
-router.route('/').get((req,res) => {
-    res.send(`Profile rout`)
-})
+// @route           GET api/profile/me
+// @description     Get current users profile
+// @access          Private
+router.get('/me', auth, async (req, res) => {
+    try {
+        const profile = await Profile.findOne({
+            user: req.user.id,
+        }).populate({
+            path: 'user',
+            select: 'name avatar',
+        });
 
-module.exports = router
+        if (!profile) {
+            return res.status(400).json({
+                success: false,
+                msg: `There is no profile for this user`,
+            });
+        }
+
+        return res.status(200).json({
+            success: true,
+            data: profile,
+        });
+    } catch (e) {
+        console.error(e.message);
+        return res.status(200).json({
+            success: false,
+            msg: 'Server error',
+        });
+    }
+});
+
+module.exports = router;
