ef296014abfe60a9e9473ad2a0413958216f7e89
RASEM0N <70789810+RASEM0N@users.noreply.github.com>
2021-02-27 16:17:34 +0500

    Add & delete comment

--- backend/routes/api/posts.js
+++ backend/routes/api/posts.js
@@ -242,4 +242,107 @@ router.put('/unlike/:post_id', auth, async (req, res) => {
     }
 });
 
+// @route           POST api/posts/comment/:post_id
+// @description     Comment on a post
+// @access          Private
+router.post(
+    '/comment/:post_id',
+    [auth, body('text', 'Text is required').not().isEmpty()],
+    async (req, res) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({
+                success: true,
+                errors: errors.array(),
+            });
+        }
+
+        try {
+            const post = await Post.findById(req.params.post_id);
+            const user = await User.findById(req.user.id);
+            if (!post && !user) {
+                return res.status(401).json({
+                    success: false,
+                    msg: 'Post or user not found',
+                });
+            }
+
+            post.comments.unshift({
+                user: req.user.id,
+                text: req.body.text,
+                name: user.name,
+                avatar: user.avatar,
+            });
+
+            await post.save();
+
+            await res.status(200).json({
+                succcess: true,
+                data: post,
+            });
+        } catch (e) {
+            console.log(e.message);
+            if (e.kind === 'ObjectId') {
+                return res.status(404).json({
+                    success: false,
+                    msg: 'Post not found',
+                });
+            }
+            await res.status(500).json({
+                success: false,
+                msg: `Server error`,
+            });
+        }
+    }
+);
+
+// @route           DELETE api/posts/comment/:post_id/:comment_id
+// @description     Delete comment
+// @access          Private
+router.delete('/comment/:post_id/:comment_id', auth, async (req, res) => {
+    try {
+        const post = await Post.findById(req.params.post_id);
+
+        const comment = post.comments.find(({ _id }) => {
+            return _id.toString() === req.params.comment_id;
+        });
+
+        if (!comment) {
+            return res.status(404).json({
+                success: true,
+                msg: 'Comment does not exist',
+            });
+        }
+
+        if (comment.user.toString() !== req.user.id) {
+            return res.status(404).json({
+                success: false,
+                msg: 'User not authorized',
+            });
+        }
+
+        post.comments = post.comments.filter(({ _id }) => {
+            return _id.toString() !== req.params.comment_id;
+        });
+
+        await post.save();
+
+        await res.status(200).json({
+            succcess: true,
+            data: post,
+        });
+    } catch (e) {
+        console.log(e.message);
+        if (e.kind === 'ObjectId') {
+            return res.status(404).json({
+                success: false,
+                msg: 'Post not found',
+            });
+        }
+        await res.status(500).json({
+            success: false,
+            msg: `Server error`,
+        });
+    }
+});
 module.exports = router;
