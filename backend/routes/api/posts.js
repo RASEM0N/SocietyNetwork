36270ef2499464376c11b2bec60fb61c0b56a4ec
RASEM0N <70789810+RASEM0N@users.noreply.github.com>
2021-02-27 15:05:34 +0500

    Get and delete post by id

--- backend/routes/api/posts.js
+++ backend/routes/api/posts.js
@@ -54,4 +54,96 @@ router.post(
     }
 );
 
+// @route           GET api/posts
+// @description     GET all posts
+// @access          Private
+router.get('/', auth, async (req, res) => {
+    try {
+        const posts = await Post.find();
+
+        await res.status(200).json({
+            success: true,
+            data: posts,
+        });
+    } catch (e) {
+        console.error(e.message);
+        await res.status(500).json({
+            success: false,
+            msg: `Server error`,
+        });
+    }
+});
+
+// @route           GET api/posts/:post_id
+// @description     Get post by ID
+// @access          Private
+router.get('/:post_id', auth, async (req, res) => {
+    try {
+        const post = await Post.findById(req.params.post_id);
+
+        if (!post) {
+            return res.status(401).json({
+                success: false,
+                msg: 'Post not found',
+            });
+        }
+
+        await res.status(200).json({
+            success: true,
+            data: post,
+        });
+    } catch (e) {
+        console.log(e.message);
+
+        if (e.kind === 'ObjectId') {
+            return res.status(404).json({
+                success: false,
+                msg: 'Post not found',
+            });
+        }
+
+        await res.status(500).json({
+            success: false,
+            msg: `Server error`,
+        });
+    }
+});
+
+// @route           DELETE api/posts/:post_id
+// @description     Delete a post
+// @access          Private
+router.delete('/:post_id', auth, async (req, res) => {
+    try {
+        const post = await Post.findById(req.params.post_id);
+
+        if (post.user.toString() !== req.user.id) {
+            return res.status(401).json({
+                success: false,
+                msg: "Don't have permission to delete",
+            });
+        }
+
+        await post.remove();
+
+        await res.status(200).json({
+            success: true,
+            data: {},
+        });
+    } catch (e) {
+        console.log(e.message);
+
+        if (e.kind === 'ObjectId') {
+            return res.status(404).json({
+                success: false,
+                msg: 'Post not found',
+            });
+        }
+
+        await res.status(500).json({
+            success: false,
+            msg: `Server error`,
+        });
+    }
+});
+
 module.exports = router;
