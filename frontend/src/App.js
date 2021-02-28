cf0c3f333052a959d9855ff513847b942871d79b
RASEM0N <70789810+RASEM0N@users.noreply.github.com>
2021-03-01 04:18:18 +0500

    React Router. Link. Route

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,27 @@
+/* eslint-disable */
 import React, { Fragment } from 'react';
 import './app.css';
 import Navbar from './components/layout/Navbar';
 import Landing from './components/layout/Landing';
+import { BrowserRouter as _Router_, Route, Switch } from 'react-router-dom';
+import Login from './components/auth/login';
+import Register from './components/auth/register';
 
 function App() {
     return (
-        <Fragment>
-            <Navbar />
-            <Landing />
-        </Fragment>
+        <_Router_>
+            <Fragment>
+                <Navbar />
+                <Route exact path="/" component={Landing} />
+
+                <section className="container">
+                    <Switch>
+                        <Route exact path="/login" component={Login} />
+                        <Route exact path="/register" component={Register} />
+                    </Switch>
+                </section>
+            </Fragment>
+        </_Router_>
     );
 }
 
