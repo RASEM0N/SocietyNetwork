7e02014106422415f3a1d3e4e4a55bb1f3d357af
RASEM0N <70789810+RASEM0N@users.noreply.github.com>
2021-03-01 16:08:45 +0500

    Login Form

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,13 +3,13 @@ import React, { Fragment } from 'react';
 import './app.css';
 import Navbar from './components/layout/Navbar';
 import Landing from './components/layout/Landing';
-import { BrowserRouter as _Router_, Route, Switch } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Login from './components/auth/login';
 import Register from './components/auth/register';
 
 function App() {
     return (
-        <_Router_>
+        <BrowserRouter>
             <Fragment>
                 <Navbar />
                 <Route exact path="/" component={Landing} />
@@ -21,8 +21,13 @@ function App() {
                     </Switch>
                 </section>
             </Fragment>
-        </_Router_>
+        </BrowserRouter>
     );
 }
+
+/* ----------LINKS ON MY QUESTIONS-------------
+ * switch, exact:
+ * https://stackoverflow.com/questions/45122800/react-router-switch-behavior
+ * */
 
 export default App;
--- frontend/src/components/auth/register.js
+++ frontend/src/components/auth/register.js
@@ -1,4 +1,5 @@
 import React, { Fragment, useState } from 'react';
+import { Link } from 'react-router-dom';
 
 const Register = ({ ...props }) => {
     const [formData, setForm] = useState({
@@ -12,12 +13,13 @@ const Register = ({ ...props }) => {
 
     const onChange = (e) =>
         setForm({ ...formData, [e.target.name]: e.target.value });
-    const onSubmit = (e) => {
+
+    const onSubmit = async (e) => {
         e.preventDefault();
         if (password !== password2) {
             console.log(`Password do not match`);
         } else {
-            console.log(formData);
+            console.log('Success');
         }
     };
 
@@ -78,7 +80,7 @@ const Register = ({ ...props }) => {
                 />
             </form>
             <p className="my-1">
-                Already have an account? <a href="login.html">Sign In</a>
+                Already have an account? <Link to="/login">Sign In</Link>
             </p>
         </Fragment>
     );
